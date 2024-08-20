28720340381f7a161f1b702db783048103486f6c
Karen Baltazar <a01707750@tec.mx>
2024-08-20 17:15:45 -0600

    Update title, favicon, and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': {
+          50: '#faf8ff',
+          100: '#f5f2ff',
+          200: '#f1ebff',
+          300: '#ece4ff',
+          400: '#e7deff',
+          500: '#e2d7ff',
+          600: '#ded0ff',
+          700: '#d9caff',
+          800: '#d5c3ff',
+          900: '#d0bcff',
+        },
+        'gray' : {
+          50: '#e2e1e3',
+          100: '#c5c4c7',
+          300: '#8e8d92',
+          600: '#424148',
+          900: '#05040a',
+        },
+      },
+    },
   },
   plugins: [],
 }
