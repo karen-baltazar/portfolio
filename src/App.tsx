c01ea78d3758563ff9f55e731dd6ca9df5f65abe
Karen Baltazar <a01707750@tec.mx>
2024-08-22 17:04:00 -0600

    Integrate  About into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="bg-gray-900 md:px-16">
+    <div className="bg-gray-900 md:px-16 w-full h-full">
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 }
