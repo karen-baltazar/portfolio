ab638483c9ac0ceca8970df5cdcc5ca2781b84ee
Karen Baltazar <a01707750@tec.mx>
2024-08-23 11:13:12 -0600

    Integrate Projects into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import About from "./components/About";
-import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Projects />
       <Contact />
     </div>
   );
